58c62ebc3a41696d17b886929b3f1d9c94c03abb
Knut Fredrik Torp <Knut.Fredrik.Torp@inmeta.no>
2019-02-28 16:02:02 +0100

    Created at homepage, resource module and resource-list.component

---
+++ Catapult/src/app/resources/resourse-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'crb-resourse-list',
+  templateUrl: './resourse-list.component.html',
+  styleUrls: ['./resourse-list.component.scss']
+})
+export class ResourseListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
